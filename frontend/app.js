786c918a94d5c7abb03bb24d261a5cba3121b8e0
Lobinda Wang <winer909@gmail.com>
2026-08-17 14:01:13 +0800

    fixed: api add headers

--- frontend/app.js
+++ frontend/app.js
@@ -297,6 +297,7 @@ async function submitData() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "ngrok-skip-browser-warning": "true",
       },
       body: JSON.stringify({
         lineUserId: state.lineUserId,
